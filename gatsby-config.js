7faa7ff6ebbd98c73b29e6788fb87c2e41709fce
Kotlia <62182668+Kotlia@users.noreply.github.com>
2020-11-20 04:54:58 -0500

    🔼 Github url

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,8 +17,7 @@ module.exports = {
       options: {
         configPath: `src/config`,
         docsPath: `src/docs`,
-        githubUrl: `https://github.com/rocketseat/gatsby-themes`,
-        baseDir: `examples/gatsby-theme-docs`,
+        githubUrl: `https://github.com/OyasaiServer/wiki`,
       },
     },
     {
